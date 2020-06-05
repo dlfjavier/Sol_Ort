b2a06b8eb22b5239480cde86ccf60a2e0140f3f7
JAVIER DE LA FUENTE <16599452@comunidadort.edu.ar>
2020-06-05 20:22:08 -0300

    Add project files.

---
+++ Sol_Ort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sol_Ort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
